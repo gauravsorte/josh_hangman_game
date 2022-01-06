e4e795741eccd8781ff9a246152330c1b9b8b430
gauravsorte <sortegaurav9@gmail.com>
2022-01-06 19:35:11 +0530

    added more comments and more easy sounding variable names for easier understanding

--- main.py
+++ main.py
@@ -8,43 +8,53 @@ import requests
 import json
 import numpy as np
 
+
 def hangman():
     response_api = requests.get('https://random-word-api.herokuapp.com/word?number=1')
     text = response_api.text
     j_text = json.loads(text)[0]
-
+    # selecting hints according to the number of letter in the word.
     length_of_text = len(j_text)
-    if(length_of_text < 6):
+    if (length_of_text < 6):
         no_of_letters = 2
-    elif(length_of_text < 9):
+    elif (length_of_text < 9):
         no_of_letters = 3
-    elif(length_of_text < 11):
+    elif (length_of_text < 11):
         no_of_letters = 4
     else:
         no_of_letters = 5
 
-    values = np.random.randint(0, length_of_text, size = (no_of_letters))
+    # selecting random positions of the hints. 
+    values = np.random.randint(0, length_of_text, size=no_of_letters)
+    # just a copy of original text/word
     duplicate_text = j_text
-    already_list = []
+    # list of already present characters as hint
+    present_letters_list = []
+    
+    # this for loop is used to convert the letters at the position 
+    # other than the letters at randomly selected position to _
     for i in range(0, length_of_text):
-         if i not in values:
-             duplicate_text = duplicate_text.replace(duplicate_text[i], '_',1)
-         else:
-             already_list.append(duplicate_text[i])
+        if i not in values:
+            duplicate_text = duplicate_text.replace(duplicate_text[i], '_', 1)
+        else:
+            present_letters_list.append(duplicate_text[i])
     # print(values)
 
-    run_hangman_game(duplicate_text, j_text, already_list, values)
-    
+    run_hangman_game(duplicate_text, j_text, present_letters_list, values)
+
+
 def run_hangman_game(converted_text, original_text, ava_words, positions):
     print(converted_text)
     # print(ava_words)
     life_count = 5
-    # list of total number of words in the text
+    
+    # list of total number of characters in the text
     words_list = split(original_text)
     # print(words_list)
-    # remove the already available word from the list
-    split_words = split_list(words_list, positions)
-    # print(split_words)
+    
+    # remove the already available characters as a hint from the list.
+    remaining_char = split_list(words_list, positions)
+    # print(remaining_char)
     while True:
 
         if (life_count == 0):
@@ -52,17 +62,16 @@ def run_hangman_game(converted_text, original_text, ava_words, positions):
             print('The correct answer was : ', original_text)
             break
         elif (converted_text == original_text):
-            print('Hurray... You Won The Game..Congrulation')
+            print('Hurray... You Won The Game..Congratulation')
             break
         else:
             inp = input('guss a word')
-            if inp in split_words:
+            if inp in remaining_char:
                 # converting all presence of the typed words from _ to word
                 for i in range(len(words_list)):
-
                     if inp == words_list[i]:
-                        converted_text = converted_text[:i] + inp + converted_text[i+1:]
-                split_words.remove(inp)
+                        converted_text = converted_text[:i] + inp + converted_text[i + 1:]
+                remaining_char.remove(inp)
                 print(converted_text)
             else:
                 life_count -= 1
@@ -70,9 +79,9 @@ def run_hangman_game(converted_text, original_text, ava_words, positions):
                 print(converted_text)
 
 
-
 def split(word):
     return [word for word in word]
+
 
 def split_list(l1, pos):
     l2 = []
@@ -80,11 +89,10 @@ def split_list(l1, pos):
         if i not in pos:
             l2.append(l1[i])
     return l2
-    
+
+
 # no of letters to show
-#convert the other letters to _
-
-
+# convert the other letters to _
 
 
 # Press the green button in the gutter to run the script.
